b322526bcad8eb80c1b8035bd7ab1b35db7f804d
Yonas Mamo <63198859+yonasi@users.noreply.github.com>
2025-02-02 21:46:00 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,25 +1,22 @@
 import mysql.connector
 
-def create_database(db_name):
-    """Creates a MySQL database."""
+def create_database (db_name):
     try:
-        # Connect to MySQL server (without specifying a database initially)
+
         mydb = mysql.connector.connect(
-            host="localhost",  # Replace with your MySQL host
-            user="root",  # Replace with your MySQL username
-            password="Yn@0949753643"  # Replace with your MySQL password
+            host="localhost", 
+            user="root",
+            password="Yn@0949753643"
         )
 
         mycursor = mydb.cursor()
 
-        # Check if the database exists. If it does, try dropping it first.
         try:
-            mycursor.execute(f"DROP DATABASE IF EXISTS {db_name}")
+            mycursor.execute(f"DROP DATABASE IF EXISTS {alx_book_store}")
         except mysql.connector.Error as err:
             print(f"Error dropping database (if it existed): {err}")
 
-        # Create the database
-        mycursor.execute(f"CREATE DATABASE {db_name}")
+        mycursor.execute(f"CREATE DATABASE IF NOT EXISTS alx_book_store")
         print(f"Database '{db_name}' created successfully!")
 
     except mysql.connector.Error as err:
